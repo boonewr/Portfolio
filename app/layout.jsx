f2c764756bd8213f2f3ea6cabe384afbb1daec50
wrboone <wrboone@ncsu.edu>
2026-05-06 19:03:58 -0400

    Added analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Robbie Boone | Software Engineer",
@@ -16,7 +17,10 @@ export default function RootLayout({ children }) {
           }}
         />
       </head>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
